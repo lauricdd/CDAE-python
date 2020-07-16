7b7424f756e8e938a5eb743c7c125acd8d089090
lauricdd <lauri_cdd@hotmail.com>
2020-07-16 19:05:36 +0200

    debugging

--- src_v2/utils/data_manager.py
+++ src_v2/utils/data_manager.py
@@ -156,10 +156,6 @@ def prepare_data(data_name, DATASET_URL=None, DATASET_SUBFOLDER=None, DATASET_FI
         print(final_ratings_df)
         print("="*100)
 
-        # final columns renaming
-        final_ratings_df.columns = ['user_id', 'movie_id', 'rating']
-        print(final_ratings_df)
-
         # check correpondence between original and new IDs
         if data_name == "movielens_10m": # only for movielens_dataset since considers specific tuples
             test_movielens_10m_rescaling(ratings_df, final_ratings_df) 
@@ -269,11 +265,16 @@ def rescale_ids(ratings_df, id_name, original_id_name_is_sorted):
             final_ratings_df = pd.concat([ratings_df, id_name_sorted_df['NEW_' + id_name]], axis=1, sort=False)
 
         if id_name == 'user_id' and 'NEW_user_id' in final_ratings_df.columns: 
-                final_ratings_df = final_ratings_df[['NEW_user_id', 'movie_id', 'rating']]  
-
+            print("id_name == 'user_id' and 'NEW_user_id' in final_ratings_df.columns")
+            final_ratings_df = final_ratings_df[['NEW_user_id', 'movie_id', 'rating']]  
+            final_ratings_df.head()
+            final_ratings_df.columns = ['user_id', 'movie_id', 'rating'] # columns renaming
         
-        elif id_name == 'movie_id' and 'NEW_movie_id' in final_ratings_df.columns: 
-                final_ratings_df = final_ratings_df[['user_id', 'NEW_movie_id', 'rating']]  
+        if id_name == 'movie_id' and 'NEW_movie_id' in final_ratings_df.columns: 
+            final_ratings_df = final_ratings_df[['user_id', 'NEW_movie_id', 'rating']]  
+            print("id_name == 'movie_id' and 'NEW_movie_id' in final_ratings_df.columns")
+            final_ratings_df.head()
+            final_ratings_df.columns = ['user_id', 'movie_id', 'rating'] # columns renaming
 
 
         return final_ratings_df
